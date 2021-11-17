66880b0435b29728c696abe6d51c3ca78f021f5a
SyedAli310 <alizaidi310@gmail.com>
2021-11-17 22:44:34 +0530

    fixed nav overflow

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,11 @@ $(document).ready(function() {
         $('#mobile-nav-toggle').toggleClass('open');
 
         //show/hide menu
-        if($('.navbar').attr('aria-expanded') === 'true'){
-            $('.navbar').attr('aria-expanded', 'false');
-        } else if ($('.navbar').attr('aria-expanded') === 'false'){
-            $('.navbar').attr('aria-expanded', 'true');
-        }
+        // if($('.navbar').attr('aria-expanded') === 'true'){
+        //     $('.navbar').attr('aria-expanded', 'false');
+        // } else if ($('.navbar').attr('aria-expanded') === 'false'){
+        //     $('.navbar').attr('aria-expanded', 'true');
+        // }
+        $('.navbar').toggleClass('is-open');
     })
 });
