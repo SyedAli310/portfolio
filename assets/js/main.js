5e7126d38f3dfc699d85c2cbcd325a0df0156085
SyedAli310 <alizaidi310@gmail.com>
2021-12-04 18:05:16 +0530

    hover effect change + code formatted

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,17 @@
-
-$(document).ready(function() {
-    function checkScroll(){
-        if($(document).scrollTop() > 1){
-            $('.main-navigation').addClass('scrolled');
-        } else {
-            $('.main-navigation').removeClass('scrolled');
-        }
+$(document).ready(function () {
+  function checkScroll() {
+    if ($(document).scrollTop() > 1) {
+      $(".main-navigation").addClass("scrolled");
+    } else {
+      $(".main-navigation").removeClass("scrolled");
     }
-    checkScroll();
-    $('#mobile-nav-toggle').on('click', ()=>{
-        // animate button
-        $('#mobile-nav-toggle').toggleClass('open');
-        // toggle navigation
-        $('.navbar').toggleClass('is-open');
-    })
-    $(document).on('scroll', checkScroll)
+  }
+  checkScroll();
+  $("#mobile-nav-toggle").on("click", () => {
+    // animate button
+    $("#mobile-nav-toggle").toggleClass("open");
+    // toggle navigation
+    $(".navbar").toggleClass("is-open");
+  });
+  $(document).on("scroll", checkScroll);
 });
