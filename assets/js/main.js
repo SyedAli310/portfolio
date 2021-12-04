311b9c294cac680a372f25616c06d8431f6d9709
SyedAli310 <alizaidi310@gmail.com>
2021-12-04 22:39:05 +0530

    hero designed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-$(document).ready(function () {
+$(document).ready( () => {
+    const animatedHero = document.querySelector('#animated-text-hero');
+
   function checkScroll() {
     if ($(document).scrollTop() > 1) {
       $(".main-navigation").addClass("scrolled");
@@ -14,4 +16,19 @@ $(document).ready(function () {
     $(".navbar").toggleClass("is-open");
   });
   $(document).on("scroll", checkScroll);
+  animatedHero.style.animation = 'appear-from-middle ease 1s forwards';
+
+  function changeProfessionText() {
+    const professionText = document.querySelector('.profession-span');
+    const professionTextArray = ['Web Developer', 'Web Designer', 'UI/UX Designer', 'Graphic Designer'];
+    let index = 0;
+    setInterval( () => {
+      professionText.innerHTML = professionTextArray[index];
+      index++;
+      if (index >= professionTextArray.length) {
+        index = 0;
+      }
+    }, 2000);
+  }
+    changeProfessionText();
 });
