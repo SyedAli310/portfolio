8507af88020861c1c5d59d2ccdc3e5be370611b0
SyedAli310 <alizaidi310@gmail.com>
2021-12-13 04:13:13 +0530

    waiting for images to load before adding appear animation to cards

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
   // projects appear animation
   const projectCards = document.querySelectorAll(".project-card");
-  projectCards.forEach((card, index) => {
-    card.style.animation = "slide-in-up ease 300ms forwards";
-    card.style.animationDelay = `${100 * index}ms`;
-  });
+  window.onload = () => {
+    projectCards.forEach((card, index) => {
+        card.style.animation = "slide-in-up ease 300ms forwards";
+        card.style.animationDelay = `${100 * index}ms`;
+    });
+  }
 
   //modal open handlers
   $(".filters-btn").on("click", (e) => {
