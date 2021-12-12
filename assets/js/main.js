2844e38658d4f2aec3d11541dc93322a9675af3a
SyedAli310 <alizaidi310@gmail.com>
2021-12-13 04:18:03 +0530

    slide-in animation added for section headings

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,5 +72,8 @@ $(document).ready(() => {   //<-start of document ready->//
     }
   });
 
+  //adding animation to section-heading
+  $(".section-heading").css('animation','appear-from-left 0.5s ease-in-out forwards');
+
 //<-end of document ready->//
 });
