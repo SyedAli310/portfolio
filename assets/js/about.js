c16013c4f104359a855b34ed7a58cb6e8d99eeb1
SyedAli310 <alizaidi310@gmail.com>
2021-12-14 02:23:14 +0530

    Update about.js

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,9 +1,7 @@
 $(document).ready(() => {
-    const socialLinks = document.querySelectorAll(".social-links a");
-    window.onload = () => {
-      socialLinks.forEach((link, index) => {
-          link.style.animation = "slide-in-left ease 300ms forwards";
-          link.style.animationDelay = `${100 * index}ms`;
-      });
-    }
-}); 
+  const socialLinks = document.querySelectorAll(".social-links a");
+  socialLinks.forEach((link, index) => {
+    link.style.animation = "slide-in-left ease 300ms forwards";
+    link.style.animationDelay = `${100 * index}ms`;
+  });
+});
