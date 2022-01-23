2e190e77e3b8dc3d60adf2bedbb98c8d313bbbf9
SyedAli310 <alizaidi310@gmail.com>
2022-01-23 21:52:19 +0530

    Improved github stats section and added contributions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
   const animatedHero = document.querySelector(".animated-text-hero");
   const hiSpan = document.querySelector(".animated-text-hero .hi-span");
-  const getInTouchFormInpFields = document.querySelectorAll('.modal .input-field');
-  const smallSpinner = `<span class='spinner-sm'></span>`
+  const getInTouchFormInpFields = document.querySelectorAll(
+    ".modal .input-field"
+  );
+  const smallSpinner = `<span class='spinner-sm'></span>`;
   animatedHero.style.animation = "appear-from-middle ease 1s forwards";
   hiSpan.style.animation =
     "slide-in-left 1s ease 1s forwards, gradient-animation 3s backwards infinite";
@@ -29,16 +31,20 @@ $(document).ready(function () {
   }
   changeProfessionText();
 
-    // sendMail function
-    async function sendMail(params){
-      var tempParams = {
-          'to_email' : params.email,
-          'from_name': params.name,
-          'reply_to' : 'czuar47@gmail.com',
-      };
-      const res = await emailjs.send('service_bq0bz8q','template_iwj5q8g', tempParams)
-      return res;
-    }
+  // sendMail function
+  async function sendMail(params) {
+    var tempParams = {
+      to_email: params.email,
+      from_name: params.name,
+      reply_to: "czuar47@gmail.com",
+    };
+    const res = await emailjs.send(
+      "service_bq0bz8q",
+      "template_iwj5q8g",
+      tempParams
+    );
+    return res;
+  }
 
   //modal open handlers
   $(".get-in-touch-btn").on("click", (e) => {
@@ -46,77 +52,81 @@ $(document).ready(function () {
   });
 
   //submit get-in-touch form
-  $('#get-in-touch-form').on('submit', async (e)=>{
+  $("#get-in-touch-form").on("submit", async (e) => {
     e.preventDefault();
-    $('#modal-send-msg-btn').html(`${smallSpinner}&nbsp;Sending`);
-    const name = $('#name').val();
-    const email = $('#email').val();
-    const message = $('#message').val();
-    const data = {name ,email, message}
-    if(data && name && email && message){
-      console.log('formData',data);
-      $('#name').val('');
-      $('#email').val('');
-      $('#message').val('');
+    $("#modal-send-msg-btn").html(`${smallSpinner}&nbsp;Sending`);
+    const name = $("#name").val();
+    const email = $("#email").val();
+    const message = $("#message").val();
+    const data = { name, email, message };
+    if (data && name && email && message) {
+      console.log("formData", data);
+      $("#name").val("");
+      $("#email").val("");
+      $("#message").val("");
       const mailData = {
         email,
         name,
-      }
+      };
       const res = await sendMail(mailData);
-      setTimeout(()=>{
-        if(res){
-          $('#form-res-span').html('<h5 style="color:var(--TEXT_SUCCESS);">Thank you for contacting me</h5>');
-          $('#modal-send-msg-btn').html(`
+      setTimeout(() => {
+        if (res) {
+          $("#form-res-span").html(
+            '<h5 style="color:var(--TEXT_SUCCESS);">Thank you for contacting me</h5>'
+          );
+          $("#modal-send-msg-btn").html(`
               <span>Send</span>
               <ion-icon name="send-outline"></ion-icon>
           `);
         } else {
-          $('#form-res-span').html('<h5 style="color:var(--TEXT_DANGER);">Something went wrong. Please try again.</h5>');
-          $('#modal-send-msg-btn').html(`
+          $("#form-res-span").html(
+            '<h5 style="color:var(--TEXT_DANGER);">Something went wrong. Please try again.</h5>'
+          );
+          $("#modal-send-msg-btn").html(`
               <span>Send</span>
               <ion-icon name="send-outline"></ion-icon>
           `);
         }
-      },3500)
+      }, 3500);
     }
   });
 
   const getGithubStats = async (username) => {
     const url = `https://api.github.com/users/${username}`;
-    const res = await fetch(url)
+    const res = await fetch(url);
     const data = await res.json();
     return data;
-  }
+  };
 
-  const fillDataFromGithub = async () =>{
-    const data = await getGithubStats('SyedAli310');
-    console.log(data);
-    const avatarImg = document.querySelector('#gh-avatar');
-    const name = document.querySelector('#gh-name');
-    const bio = document.querySelector('#gh-bio');
-    const joined = document.querySelector('#gh-joined');
-    const repos = document.querySelector('#gh-repos');
+  const fillDataFromGithub = async () => {
+    const data = await getGithubStats("SyedAli310");
+    //console.log(data);
+    const avatarImg = document.querySelector("#gh-avatar");
+    const name = document.querySelector("#gh-name");
+    const bio = document.querySelector("#gh-bio");
+    const joined = document.querySelector("#gh-joined");
+    const repos = document.querySelector("#gh-repos");
     avatarImg.src = data.avatar_url;
     name.innerHTML = `${data.name} <br> <span style='font-size:smaller; opacity:0.5;'>${data.login}</span>`;
     bio.innerHTML = `<span style='color:grey;'>Bio :</span> ${data.bio}`;
-    joined.innerHTML = `<span style='color:grey;'>Joined :</span> ${new Date(data.created_at).getMonth()}/${new Date(data.created_at).getFullYear()}`;
+    joined.innerHTML = `<span style='color:grey;'>Joined :</span> ${new Date(
+      data.created_at
+    ).getMonth()}/${new Date(data.created_at).getFullYear()}`;
     repos.innerHTML = `<span style='color:grey;'>Repos :</span> ${data.public_repos}`;
-  }
-  window.onload = fillDataFromGithub()
-
+  };
+  window.onload = fillDataFromGithub();
 
   //floating input labels on focus
-  getInTouchFormInpFields.forEach((field)=>{
-    $(field).on('focus',(e)=>{
-      $(e.target).siblings(".input-label").css('transform','translateY(0)');
-      $(e.target).siblings(".input-label").css('opacity','1');
-      $(e.target).siblings(".input-label").css('visibility','visible');
-    })
-    $(field).on('blur',(e)=>{
-      $(e.target).siblings(".input-label").css('transform','translateY(50%)');
-      $(e.target).siblings(".input-label").css('opacity','0');
-      $(e.target).siblings(".input-label").css('visibility','hidden');
-    })
-  })
-
-})
+  getInTouchFormInpFields.forEach((field) => {
+    $(field).on("focus", (e) => {
+      $(e.target).siblings(".input-label").css("transform", "translateY(0)");
+      $(e.target).siblings(".input-label").css("opacity", "1");
+      $(e.target).siblings(".input-label").css("visibility", "visible");
+    });
+    $(field).on("blur", (e) => {
+      $(e.target).siblings(".input-label").css("transform", "translateY(50%)");
+      $(e.target).siblings(".input-label").css("opacity", "0");
+      $(e.target).siblings(".input-label").css("visibility", "hidden");
+    });
+  });
+});
